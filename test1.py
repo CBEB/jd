b4e5e304d59c71b3586fde7f498ff3fdaa03676f
guoguangwei <ggw@a.com>
2021-03-11 10:18:48 +0800

    miaosha qiandao

--- test1.py
+++
@@ -1,7 +0,0 @@
-print("test1")
-
-def run():
-    print("run by test1.run()")
-
-if __name__ == "__main__":
-    run()
--- test2.py
+++
@@ -1,7 +0,0 @@
-print("test2")
-
-def run():
-    print("run by test2.run()")
-
-if __name__ == "__main__":
-    run()
